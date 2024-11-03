2b2c86af08cce3db087512936127953ae08c9542
Pipiolo-Brena <143999890+Pipiolo-Brena@users.noreply.github.com>
2024-11-02 23:49:05 -0600

    Inicio del proyecto

---
+++ src/proyecto_final_poo/Proyecto_final_poo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_final_poo;
+
+/**
+ *
+ * @author PC
+ */
+public class Proyecto_final_poo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}

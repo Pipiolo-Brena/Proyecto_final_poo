2a0be4825a492cc4ab2f231893a4ffa85001970b
Milizgramo28 <manriquezliz@gmail.com>
2024-11-05 23:20:43 -0600

    atributos, constructor setters getters

--- src/Vuelos/VueloInternacional.java
+++ src/Vuelos/VueloInternacional.java
@@ -4,10 +4,44 @@
  */
 package Vuelos;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author PC
  */
 public class VueloInternacional extends Vuelo {
+    private String destinoPais;
+    private boolean requiereVisa;
+
     
+    //constructor
+    public VueloInternacional(String aerolinea, String numVuelo, String origen, String destinoPais, boolean requiereVisa, LocalDateTime fechaSalida, double precio, int disponibilidad, int escalas) {
+        super(aerolinea, numVuelo, origen, fechaSalida, precio, disponibilidad, escalas, false);
+        this.destinoPais = destinoPais;
+        this.requiereVisa = requiereVisa;
+    }
+
+    //settres getters
+    public String getDestinoPais() {
+        return destinoPais;
+    }
+
+    public void setDestinoPais(String destinoPais) {
+        this.destinoPais = destinoPais;
+    }
+
+    public boolean isRequiereVisa() {
+        return requiereVisa;
+    }
+
+    public void setRequiereVisa(boolean requiereVisa) {
+        this.requiereVisa = requiereVisa;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " con destino a " + destinoPais + (requiereVisa ? " (requiere visa)" : "");
+    }
+
 }
--- src/Vuelos/VueloNacional.java
+++ src/Vuelos/VueloNacional.java
@@ -4,10 +4,33 @@
  */
 package Vuelos;
 
+import java.time.LocalDateTime;
+
 /**
  *
  * @author PC
  */
 public class VueloNacional extends Vuelo {
+    private String destinoEstado;
+
     
+
+    public VueloNacional(String aerolinea, String numVuelo, String origen, String destinoEstado, LocalDateTime fechaSalida, double precio, int disponibilidad, int escalas) {
+        super(aerolinea, numVuelo, origen, fechaSalida, precio, disponibilidad, escalas, true);
+        this.destinoEstado = destinoEstado;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " con destino a " + destinoEstado;
+    }
+
+    //setters getters
+    public String getDestinoEstado() {
+        return destinoEstado;
+    }
+
+    public void setDestinoEstado(String destinoEstado) {
+        this.destinoEstado = destinoEstado;
+    }
 }

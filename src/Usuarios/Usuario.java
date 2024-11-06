5440e1f2a1456ebd8d93ed76cbe2f9f961987392
Milizgramo28 <manriquezliz@gmail.com>
2024-11-05 21:12:20 -0600

    atributos,  construcctor y override

--- src/Usuarios/Usuario.java
+++ src/Usuarios/Usuario.java
@@ -3,11 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Usuarios;
+import Usuarios.GestorDeAcceso;
 
 /**
  *
  * @author
  */
-public abstract class Usuario  {
+public abstract class Usuario implements GestorDeAcceso {
+    private String nombreUsuario;
+    private String contraseña;
+
+    //constructor
+    public Usuario(String nombreUsuario, String contraseña) {
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+    }
+
+    @Override
+    public abstract boolean validarContraseña(String contraseña);
+
+    @Override
+    public abstract void recuperarContraseña();
     
 }

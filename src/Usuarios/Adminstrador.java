114909ab0434d693d58f068222453cf8cbb832e3
Milizgramo28 <manriquezliz@gmail.com>
2024-11-05 22:46:50 -0600

    atributos, constructor setters getters

--- src/Usuarios/Adminstrador.java
+++ src/Usuarios/Adminstrador.java
@@ -4,10 +4,96 @@
  */
 package Usuarios;
 
+import java.util.List;
+
+import Sistema.GestionReservas;
+import Vuelos.Vuelo;
+
 /**
  *
  * @author PC
  */
 public class Adminstrador extends Usuario {
+    private String nombre;
+    private String apellido;
+    private int numEmpleado;
+    private List<Cliente> listaClientes;//registro clientes
+    private List<Vuelo> baseVuelos; //vuelos disponibles
     
+    //constructor
+    public Administrador(String nombreUsuario, String contraseña, String nombre, String apellido, int numEmpleado) {
+        super(nombreUsuario, contraseña);
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numEmpleado = numEmpleado;
+    
+    }
+
+    @Override
+    public boolean validarContraseña(String contraseña) {
+        return this.contraseña.equals(contraseña);
+    }
+
+    @Override
+    public void recuperarContraseña() {
+        
+    }
+
+    public void verVuelosDisponibles() {
+        baseVuelos.mostrarVuelos();
+    }
+
+    public void agregarVuelos(Vuelo vuelo) {
+        baseVuelos.registrarVuelo(vuelo);
+    }
+
+    public void eliminarVuelos(String numVuelo) {
+        baseVuelos.eliminarVuelo(numVuelo);
+    }
+
+    public void verInformacionClientes() {
+        
+    }
+
+    public void eliminarCliente(Cliente cliente) {
+        listaClientes.remove(cliente);
+    }
+
+    //getters setters
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getNumEmpleado() {
+        return numEmpleado;
+    }
+
+
+    public void setNumEmpleado(int numEmpleado) {
+        this.numEmpleado = numEmpleado;
+    }
+
 }

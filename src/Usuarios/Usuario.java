136ab11848a07a517cc251d56f8979a6e20e5381
Crobvv <143209957+Crobvv@users.noreply.github.com>
2024-11-15 18:46:22 -0600

    Update Usuario.java

--- src/Usuarios/Usuario.java
+++ src/Usuarios/Usuario.java
@@ -3,11 +3,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Usuarios;
-import Usuarios.GestorDeAcceso;
 
 /**
- *
- * @author
+ * Clase abstracta que implementa la interfaz GestorDeAcceso y define atributos y método para
+ * un usuario cualquiera en el sistema.
+ * 
+ * @author Equipo 5
+ * @version 2024.11.15
  */
 public abstract class Usuario implements GestorDeAcceso {
     protected String nombreUsuario;
@@ -16,31 +18,60 @@ public abstract class Usuario implements GestorDeAcceso {
     private String nombre;
     private String apellido;
 
-    //constructor
-    public Usuario(String numCliente, String nombre, String apellido ,String nombreUsuario, String contraseña) {
+    /**
+     * Constructor de clase que inicializa todos los atributos.
+     * 
+     * @param nombreUsuario
+     * @param contraseña
+     * @param numCliente
+     * @param nombre
+     * @param apellido
+     */
+    public Usuario(String nombreUsuario, String contraseña, String numCliente, String nombre, String apellido) {
         this.nombreUsuario = nombreUsuario;
         this.contraseña = contraseña;
-        this.numCliente= numCliente;
+        this.numCliente = numCliente;
         this.nombre = nombre;
         this.apellido = apellido;
     }
 
     public String getNumCliente() {
-        return numCliente;
+        return numCliente; // dado que el número de cliente no cambia una vez creado, solo se define su getter
     }
 
+    // Getters y setters para el resto de atributos
     public String getContraseña() {
         return contraseña;
+    }
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
     }
 
     public String getNombreUsuario() {
         return nombreUsuario;
     }
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    // Los métodos abstractos se definen en las clases concretas
     @Override
     public abstract boolean validarContraseña(String contraseña);
 
     @Override
-    public abstract void recuperarContraseña();
-    
+    public abstract void recuperarContraseña();   
 }

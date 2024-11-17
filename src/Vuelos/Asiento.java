e3d5b9a61b6aaf171ab4500675b0aa07c0353b32
Crobvv <143209957+Crobvv@users.noreply.github.com>
2024-11-17 09:09:20 -0600

    Comentarios de clase y modificaciones menores

--- src/Vuelos/Asiento.java
+++ src/Vuelos/Asiento.java
@@ -1,16 +1,31 @@
 package Vuelos;
 
-public class Asiento {
-    private String numero;   
-    private boolean esVIP;     
-    private boolean ocupado;     
+/**
+ * Representa un asiento de un vuelo.
+ * 
+ * @author Equipo 5
+ * @version 2024.11.17
+ */
 
+public class Asiento {
+    private String numero; // número del asiento
+    private boolean esVIP; // indica si es VIP
+    private boolean ocupado; // indica si está ocupado
+
+    /**
+     * Constructor para crear un asiento.
+     * 
+     * @param numero
+     * @param esVIP
+     */
     public Asiento(String numero, boolean esVIP) {
         this.numero = numero;
         this.esVIP = esVIP;
         this.ocupado = false;
     }
 
+    // Getters
+    /** Se declaran getters para todos los atributos. */ 
     public String getNumero() {
         return numero;
     }
@@ -23,33 +38,27 @@ public class Asiento {
         return ocupado;
     }
 
+    /** Reserva el asiento si está disponible. */
     public void reservar() {
-        if (ocupado) {
-            System.out.println("El asiento no está disponible para reserva.");
+        if(ocupado) {
+            System.out.println("El asiento no está disponible para reservar.");
             return;
         }
         ocupado = true;
     }
 
+    /** Cancela la reserva del asiento si está reservado. */
     public void cancelar() {
-        if (!ocupado) {
+        if(!ocupado) {
             System.out.println("El asiento no está reservado, no se puede cancelar.");
             return;
         }
         ocupado = false;
-    }
-
-    public void liberar() {
-        if (!ocupado) {
-            System.out.println("El asiento ya está disponible.");
-            return;
-        }
-        ocupado = false;
-    }
+    } // Eliminé liberar porque hacia lo mismo que cancelar
 
     @Override
     public String toString() {
-        return "Asiento{" +
+        return "Asiento {" +
                 "numero='" + numero + '\'' +
                 ", esVIP=" + esVIP +
                 ", ocupado=" + ocupado +

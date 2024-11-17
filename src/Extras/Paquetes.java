7e684cfb9386f1116c7bc7a788522b1719ba0443
Crobvv <143209957+Crobvv@users.noreply.github.com>
2024-11-17 09:12:23 -0600

    Comentarios de clase y agregué algunos getters y setters

--- src/Extras/Paquetes.java
+++ src/Extras/Paquetes.java
@@ -3,30 +3,53 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Extras;
+import Vuelos.Vuelo;
 
 /**
- *
- * @author PC
+ * Representa un paquete de viajes que puede incluir un vuelo y un hotel.
+ * 
+ * @author Equipo 5
+ * @version 2024.11.17
  */
-
-import Vuelos.Vuelo;
 
 public class Paquetes {
     private Vuelo vuelo;
     private Hoteles hotel;
     private double precioTotal;
 
+    /** Constructor de la clase paquetes. */
     public Paquetes(Vuelo vuelo, Hoteles hotel) {
         this.vuelo = vuelo;
         this.hotel = hotel;
-        this.precioTotal = vuelo.getPrecio() + hotel.getPrecio();
+        this.precioTotal = getPrecioTotal();
     }
 
+    /** Muestra los detalles del paquete. */
     public void mostrarDetalles() {
         System.out.println("Paquete: Vuelo + Hotel");
+        System.out.println("Vuelo:");
         vuelo.mostrarDetalles();
         System.out.println("Hotel: " + hotel.getNombre() + " - Precio: " + hotel.getPrecio());
         System.out.println("Precio Total: " + precioTotal);
     }
-}
 
+    // Getters y setters
+    /** Se definen getters y setters para todos los atributos. */
+    public double getPrecioTotal() {
+        return vuelo.getPrecio() + hotel.getPrecio();
+    }
+
+    public Vuelo getVuelo() {
+        return vuelo;
+    }
+    public void setVuelo(Vuelo vuelo) {
+        this.vuelo = vuelo;
+    }
+
+    public Hoteles getHotel() {
+        return hotel;
+    }
+    public void setHotel(Hoteles hotel) {
+        this.hotel = hotel;
+    }
+}
